dd3a27edba913c54569c93600866fe0b0996eddc
saibhotla <saibhotla@gmail.com>
2018-12-14 14:56:54 -0500

    added comment

--- src/main/java/com/cognizant/cover/CoverApplication.java
+++ src/main/java/com/cognizant/cover/CoverApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoverApplication {
+	// main method
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoverApplication.class, args);
