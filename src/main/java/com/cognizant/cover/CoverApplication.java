09c42864bb9c7f8afa6667b47c5744442218062f
saibhotla <saibhotla@gmail.com>
2018-12-14 15:16:36 -0500

    test

--- src/main/java/com/cognizant/cover/CoverApplication.java
+++ src/main/java/com/cognizant/cover/CoverApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoverApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CoverApplication.class, args);
 	}
